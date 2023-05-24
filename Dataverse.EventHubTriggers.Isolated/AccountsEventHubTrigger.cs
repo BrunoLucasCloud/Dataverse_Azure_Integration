e70519bbe6ca958d05c1870fa702c74feb0d951c
Bruno Lucas <Bruno.Lucas.cloud@outlook.com>
2023-05-24 21:47:49 +1200

    hub had classes refering to contact but saving to account entity

--- Dataverse.EventHubTriggers.Isolated/AccountsEventHubTrigger.cs
+++ Dataverse.EventHubTriggers.Isolated/AccountsEventHubTrigger.cs
@@ -5,19 +5,19 @@ using Dataverse.EventHubTriggers.Isolated.Service.Interface;
 
 namespace Dataverse.EventHubTriggers.Isolated
 {
-    public class ContactsEventHubTrigger
+    public class AccountsEventHubTrigger
     {
         private readonly ILogger _logger;
         private readonly IAccountService _accountService;
 
-        public ContactsEventHubTrigger(ILoggerFactory loggerFactory, IAccountService accountService)
+        public AccountsEventHubTrigger(ILoggerFactory loggerFactory, IAccountService accountService)
         {
-            _logger = loggerFactory.CreateLogger<ContactsEventHubTrigger>();
+            _logger = loggerFactory.CreateLogger<AccountsEventHubTrigger>();
             _accountService = accountService;
         }
 
         [Function("PullFromContactEntity")]
-        public async Task Run([EventHubTrigger("contacts", Connection = "conn")] string[] events, ILogger log)
+        public async Task Run([EventHubTrigger("accounts", Connection = "conn")] string[] events, ILogger log)
         {
             foreach (string eventData in events)
             {
